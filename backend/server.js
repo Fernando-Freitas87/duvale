b6f1ea5f6750ab1f3c1dfd91619b5bfba2f4b9af
Fernando-Freitas87 <calsadosfreitas@gmail.com>
2025-02-03 17:19:56 -0300

    Descrição clara das alterações

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,11 @@
  * Importação de Módulos
  */
 const express = require('express');
-const cors = require('cors');
-const cron = require('node-cron');
+app.use(cors({
+  origin: 'https://fernando-freitas87.github.io', // Substitua pelo domínio exato do GitHub Pages
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));const cron = require('node-cron');
 const path = require('path');
 const db = require('./db');             // Conexão com o banco de dados
 const logger = require('./utils/logger'); 
@@ -45,7 +48,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'frontend')));
+//app.use(express.static(path.join(__dirname, 'frontend')));
 
 /**
  * Teste de Conexão com o Banco
