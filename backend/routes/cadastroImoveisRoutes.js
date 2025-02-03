f28e155b09e0c4faf8cf1ad545194c2f5c89daa5
Fernando Freitas <fernandofreitas@MacBook-Air-de-Fernando.local>
2025-02-02 23:13:41 -0300

    Adicionando configuração do GitHub Pages

--- backend/routes/cadastroImoveisRoutes.js
+++ backend/routes/cadastroImoveisRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CadastroImoveisController = require('../controllers/CadastroImoveisController');
+const cadastroImoveisController = require('../controllers/CadastroImoveisController');
 const db = require('../db');  // Conexão com o banco de dados
 
 router.post('/', cadastroImoveisController.cadastrarImovel);
