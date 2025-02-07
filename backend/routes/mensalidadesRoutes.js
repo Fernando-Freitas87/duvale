1f542853108608532e301d662a58c481cb8b3445
Fernando-Freitas87 <calsadosfreitas@gmail.com>
2025-02-07 19:17:22 -0300

    “Atualizado”

--- backend/routes/mensalidadesRoutes.js
+++ backend/routes/mensalidadesRoutes.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mensalidadesController = require('../controllers/mensalidadesController');
+const mensalidadesController = require('../controllers/mensalidadesController'); // Adicione esta linha
 const { authenticateToken } = require("../middlewares/auth");
 
-// Aplica autenticação a todas as rotas
-router.use(authenticateToken);
-
 // Rotas existentes no controlador mensalidadesController
-// Obtém os avisos gerenciais relacionados às mensalidades
 router.get('/avisos', mensalidadesController.getAvisosGerenciais);
-
-// Retorna uma lista de mensalidades em atraso
 router.get('/em-atraso', mensalidadesController.getAtrasados);
-
-// Retorna uma lista de mensalidades a vencer
 router.get('/a-vencer', mensalidadesController.getAVencer);
-
-// Fornece um resumo das mensalidades
 router.get('/resumo', mensalidadesController.getResumo);
-
-// Atualiza o status de uma mensalidade específica
 router.put('/:id/status', mensalidadesController.atualizarStatusMensalidade);
 
 module.exports = router;
