015ed5741b4d5515fb817e616ea9cda14c9a4208
Fernando-Freitas87 <calsadosfreitas@gmail.com>
2025-02-11 00:00:46 -0300

    “Atualizado”

--- backend/routes/clientesRoutes.js
+++ backend/routes/clientesRoutes.js
@@ -1,32 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authenticateToken } = require('../middlewares/auth'); // Middleware de autenticação
-const clientesController = require('../controllers/clientesController'); // Controlador de clientes
+const { getDadosBasicosCliente } = require("../controllers/clienteController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-// ---------------------------------------------------------------------
-// Rotas relacionadas aos clientes
-// ---------------------------------------------------------------------
+// Rota GET /api/cliente/dados-basicos
+router.get("/dados-basicos", authMiddleware, getDadosBasicosCliente);
 
-// 1. Obter informações do usuário autenticado
-// Endpoint: GET /api/clientes/info
-router.get('/info', authenticateToken, clientesController.getUserInfo);
-router.get('/:id', authenticateToken, clientesController.obterClientePorId);
-
-// 2. Buscar avisos do cliente autenticado
-// Endpoint: GET /api/clientes/avisos
-router.get('/avisos', authenticateToken, clientesController.getAvisos);
-
-// 3. Listar todos os clientes (apenas administradores)
-// Endpoint: GET /api/clientes/listar
-//router.get('/listar', authenticateToken, clientesController.listarClientes);
-
-// 4. lista todos os clientes
-// Endpoint: GET /api/clientes
-router.get('/', authenticateToken, clientesController.listarClientes);
-
-
-
-// ---------------------------------------------------------------------
-// Exporta o roteador
-// ---------------------------------------------------------------------
 module.exports = router;
