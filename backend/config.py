edfbb041158b17ba3c3d72d07290628cbc2939d1
Fernando-Freitas87 <calsadosfreitas@gmail.com>
2025-03-06 18:39:18 -0300

    “Atualizado”

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config:
+    # 🔥 Substituir pela chave real
+    API_MERCADO_PAGO = "APP_USR-5144563558873602-030509-baa68bd07276be53192c0d168df66e95-36887024"
+    BASE_URL = "https://api.mercadopago.com/v1"  # URL base da API
